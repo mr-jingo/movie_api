9e9e653dc71d3365094a4ab48a8ac0e4b413b40b
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-09-30 18:54:59 +0200

    task 2.4

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Conan the Barbarian',
+    director: 'John Milius'
+  },
+  {
+    title: 'Aliens',
+    director: 'James Cameron'
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'American Beauty',
+    director: 'Sam Mendes'
+  },
+  {
+    title: 'High Fidelity',
+    director: 'Stephen Frears'
+  },
+  {
+    title: 'Staying Alive',
+    director: 'Sylvester Stallone'
+  },
+  {
+    title: 'Matrix',
+    director: 'Die Wachowskis'
+  },
+  {
+    title: 'The Big Lebowski',
+    director: 'Joel Coen'
+  },
+  {
+    title: 'Indiana Jones and the Temple of Doom',
+    director: 'Steven Spielberg'
+  },
+  {
+    title: 'The Empire Strikes Back',
+    director: 'Irvin Kershner'
+  }
+];
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my myFlix!');
+});
+
+app.get('/books', (req, res) => {
+  res.json(topMovies);
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
